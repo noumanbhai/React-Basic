bebbc1db69f1bf0043c59ae1899f043aea428750
Saad bin Abid <saad.abid@ilsainteractive.com>
2021-09-13 19:44:35 +0500

    function component

---
+++ src/components/First.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// function First()
+// {
+//     return <h1>Nouman</h1>
+// }
+
+// Arrow function
+
+ const First=()=>
+     <h1>Nouman</h1>
+
+
+export default First;
