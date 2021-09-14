612393896b51aa2ffc0abd519ee8232f736a6a82
Saad bin Abid <saad.abid@ilsainteractive.com>
2021-09-14 18:19:51 +0500

    map function array value get

--- src/index.js
+++ src/index.js
@@ -3,63 +3,41 @@ import ReeactDom from "react-dom";
 //   css
 import "./style.css";
 
-const firstBook = {
-  title: "This is a image title",
-  author: "Nouman",
-  work: "Developer",
-  age: 55,
-  img: "https://m.media-amazon.com/images/I/51rErkw+OyL.jpg",
-};
-const secndBook = {
-  title: "This is a book 2 title",
-  author: "Ehsan",
-  work: "Programmer",
-  age: 88,
-  img: "https://images-na.ssl-images-amazon.com/images/I/51Mx2wNFgKL._SX379_BO1,204,203,200_.jpg",
-};
+const Books = [
+  {
+    title: "This is a image title",
+    author: "Nouman",
+    work: "Developer",
+    age: 55,
+    img: "https://m.media-amazon.com/images/I/51rErkw+OyL.jpg",
+  },
+  {
+    title: "This is a book 2 title",
+    author: "Ehsan",
+    work: "Programmer",
+    age: 88,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51Mx2wNFgKL._SX379_BO1,204,203,200_.jpg",
+  },
+];
+const names = ["nouman", "ehsan", "shoaib"];
+const newNames = names.map((name) => {
+  return <p>{name}</p>;
+});
 const Booklist = () => {
-  return (
-    <section className="booklist">
-      <Book
-        title={firstBook.title}
-        auth={firstBook.author}
-        work={firstBook.work}
-        age={firstBook.age}
-        img={firstBook.img}
-      >
-        <p>
-          t is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. The point of
-          using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters, as opposed to using 'Content here, content here', making it
-          look like readable English.
-        </p>
-      </Book>
-
-      <Book
-        title={secndBook.title}
-        auth={secndBook.author}
-        work={secndBook.work}
-        age={secndBook.age}
-        img={secndBook.img}
-      />
-      {/* <Book title="book" page={22} /> */}
-    </section>
-  );
+  return <section className="booklist">{newNames}</section>;
 };
 
-const Book = ({ title, auth, work, age, img, children }) => {
+const Book = (props) => {
   // same work
   // const Book = (props) => {
-  // const { title, auth, work, age, img } = props;
+  const { title, auth, work, age, img } = props;
   return (
     <article className="book">
       <img src={img} alt="this image not found" />
-      <p>{title}</p>
-      <p>{auth}</p>
-      <p>{work}</p>
-      <p>{age}</p>
-      <p>{children}</p>
+      <p>{props.title}</p>
+      <p>{props.auth}</p>
+      <p>{props.work}</p>
+      <p>{props.age}</p>
     </article>
   );
 };
