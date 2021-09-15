806360b097f101bfb976cfdf729058cc0b11d6ac
Saad bin Abid <saad.abid@ilsainteractive.com>
2021-09-15 13:06:05 +0500

    export component and its use

---
+++ src/components/Books.js
@@ -0,0 +1,23 @@
+export const data = [
+  {
+    title: "This is a image title",
+    author: "Nouman",
+    work: "Developer",
+    age: 55,
+    img: "https://m.media-amazon.com/images/I/51rErkw+OyL.jpg",
+  },
+  {
+    title: "This is a book 2 title",
+    author: "Ehsan",
+    work: "Programmer",
+    age: 88,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51Mx2wNFgKL._SX379_BO1,204,203,200_.jpg",
+  },
+  {
+    title: "This is a book 3 title",
+    author: "Shoaib",
+    work: "Designer",
+    age: 44,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51Mx2wNFgKL._SX379_BO1,204,203,200_.jpg",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -2,35 +2,20 @@ import React from "react";
 import ReeactDom from "react-dom";
 //   css
 import "./style.css";
-
-const Books = [
-  {
-    title: "This is a image title",
-    author: "Nouman",
-    work: "Developer",
-    age: 55,
-    img: "https://m.media-amazon.com/images/I/51rErkw+OyL.jpg",
-  },
-  {
-    title: "This is a book 2 title",
-    author: "Ehsan",
-    work: "Programmer",
-    age: 88,
-    img: "https://images-na.ssl-images-amazon.com/images/I/51Mx2wNFgKL._SX379_BO1,204,203,200_.jpg",
-  },
-];
+import { data } from "./components/Books";
 
 const Booklist = () => {
   return (
     <section className="booklist">
-      {Books.map((book, index) => {
+      {data.map((book, index) => {
         return <Book key={index} {...book}></Book>;
       })}
     </section>
   );
 };
 const clickHandler = (e) => {
-  alert(e.target);
+  console.log(e);
+  // alert(e.target);
 };
 const complexExample = (author) => {
   console.log(author);
@@ -43,7 +28,8 @@ const Book = (props) => {
     <article
       className="book"
       onMouseOver={() => {
-        alert(title);
+        console.log(title);
+        // alert(title);
       }}
     >
       <img src={img} alt="this image not found" />
