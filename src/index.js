26bd4d2b32cdceab35548e0ba18aeaf026a5e5b8
Saad bin Abid <saad.abid@ilsainteractive.com>
2021-09-15 12:03:51 +0500

    onmouse over use in function

--- src/index.js
+++ src/index.js
@@ -29,18 +29,30 @@ const Booklist = () => {
     </section>
   );
 };
-
+const clickHandler = (e) => {
+  alert(e.target);
+};
+const complexExample = (author) => {
+  console.log(author);
+};
 const Book = (props) => {
   // same work
   // const Book = (props) => {
-  const { title, auth, work, age, img } = props;
+  const { title, author, work, age, img } = props;
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        alert(title);
+      }}
+    >
       <img src={img} alt="this image not found" />
-      <p>{title}</p>
-      <p>{auth}</p>
+      <p onClick={() => console.log(title)}>{title}</p>
+      <p>{author}</p>
       <p>{work}</p>
       <p>{age}</p>
+      <button onClick={clickHandler}>Click</button>
+      <button onClick={() => complexExample(author)}>Comples</button>
     </article>
   );
 };
